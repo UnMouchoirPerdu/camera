bb55405ad3dbbdf9c5643d6046697b3fb26fdb97
Gaya Bourquard <70138303+UnMouchoirPerdu@users.noreply.github.com>
2022-05-24 10:44:48 +0200

    Test first build

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -9,21 +9,43 @@
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 3</ion-title>
-        </ion-toolbar>
+            </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 3 page" />
+
+      <div id="cameraPreview" class="cameraPreview" style="text-align: center;">
+        <ion-button @click="openCamera()" shape="round" id="openCam">Ouvrir la caméra</ion-button>
+      </div>
+
+      <div style="text-align: center;">
+        <ion-button shape="round" id="takePhoto">Prendre la photo</ion-button>
+        <ion-button @click="closeCamera()" shape="round">Fermer la caméra</ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
 
 export default defineComponent({
   name: 'Tab3Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  methods: {
+    openCamera() {
+        const cameraPreviewOptions: CameraPreviewOptions = {
+          position: 'front',
+          parent: 'cameraPreview',
+          className: 'cameraPreview'
+        };
+      CameraPreview.start(cameraPreviewOptions);
+      console.log('feur');
+  },
+    closeCamera() {
+        CameraPreview.stop();
+  }
+  }
 });
 </script>
+
